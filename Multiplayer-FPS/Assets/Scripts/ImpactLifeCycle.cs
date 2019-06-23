28f5705e8b0e9ec6dfb4359249c3a30cbb9d5893
zjpdasdf <jin.zhang.liverpool@gmail.com>
2019-06-23 11:38:42 +0100

    Multiplayer-FPS demo
    descripition can be found at https://unitylist.com/p/ev1/Multiplayer-FPS

---
+++ Multiplayer-FPS/Assets/Scripts/ImpactLifeCycle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+
+public class ImpactLifeCycle : MonoBehaviour {
+
+    [SerializeField]
+    private float lifespan = 1.5f;
+
+    private ParticleSystem particleEffect;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start() {
+        GetComponent<ParticleSystem>().Play();
+        Destroy(gameObject, lifespan);
+    }
+
+}
