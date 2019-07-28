3c56db77db8fa3aee41ec76b6923f1c2b2ed4de3
Jin Zhang <727914092@qq.com>
2019-07-28 23:55:36 +0000

    Update PlayerScore.cs

--- Multiplayer-FPS/Assets/Scripts/PlayerScore.cs
+++ Multiplayer-FPS/Assets/Scripts/PlayerScore.cs
@@ -1,5 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System;
+using System.Text;
+using System.Threading;
+using System.Net.WebSockets;
 
 // TODO: finish score system later, this script is not used now.
 public class PlayerScore : MonoBehaviour {
@@ -8,8 +12,29 @@ public class PlayerScore : MonoBehaviour {
     private int killed = 0;
     // private string notification;
 
+    // WebSocket configuration
+    Uri u = new Uri("ws://169.254.243.241:1880/ws/simple");
+    ClientWebSocket cws = null;
+    ArraySegment<byte> buf = new ArraySegment<byte>(new byte[1024]);
+
+
+    async void Connect()
+    {
+        cws = new ClientWebSocket();
+        try
+        {
+            await cws.ConnectAsync(u, CancellationToken.None);
+            if (cws.State == WebSocketState.Open) Debug.Log("connected");
+            // SayHello();
+            // GetStuff();
+            PlayerScoredSignal();
+        }
+        catch (Exception e) { Debug.Log("woe " + e.Message); }
+    }
+
     public void IncreaseKillCount() {
         killed++;
+        Connect();
         switch (killed) {
         case 1:
             score += 10;
@@ -37,5 +62,14 @@ public class PlayerScore : MonoBehaviour {
     public void AddScore(int newScore) {
         score += newScore;
     }
+
+    void PlayerScoredSignal()
+    {
+        // Set the message used to determine that the RED LED will be activated on the breadboard
+        ArraySegment<byte> b = new ArraySegment<byte>(Encoding.UTF8.GetBytes("Player get score..."));
+        cws.SendAsync(b, WebSocketMessageType.Text, true, CancellationToken.None);
+        Debug.Log("send msg");
+    }
+
 
 }
